1af0d3f56c4ca6824f09ee2834702b992f021f73
Josh Cheek <josh.cheek@gmail.com>
2019-06-09 12:17:55 -0500

    Refactor index.js a little bit

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const WebSocket = require('ws')
 const readline = require('readline')
 
 const pid = parseInt(process.argv[2])
-console.log(pid)
+process._debugEnd()
 process._debugProcess(pid)
 
-const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+const rl = readline.createInterface({input: process.stdin, output: process.stdout})
+let ws
+run()
+  .catch((err) => console.error(err.stack))
+  .finally(() => rl.close())
 
-rl.question('What is the websocket? ', (wsString) => {
-  wsString = wsString.trim()
-  rl.question('What expression do you want to evaluate? ', (expression) => {
-    console.log(`EXPRESSION: ${expression}`)
-    rl.close()
-
-    const ws = new WebSocket(wsString)
-
-    ws.on('open', function open() {
-      console.log('opened')
-      const id = 0
-      const method = 'Runtime.evaluate'
-      const params = {expression: expression}
-      ws.send(JSON.stringify({id, method, params}))
-    })
-
-    ws.on('message', function incoming(data) {
-      console.log(`message: ${data}`)
-    })
+async function run() {
+  const wsString = await new Promise(resolve =>
+    rl.question('What is the websocket? ', resolve)
+  )
+  const ws = new WebSocket(wsString.trim())
+  ws.on('message', (message) => {
+    ws.close()
+    console.log(message)
   })
-})
+  await new Promise(resolve => ws.on('open', resolve))
+
+  let id = 0
+  const method = 'Runtime.evaluate'
+  while(true) {
+    const expression = await new Promise(resolve =>
+      rl.question('What expression do you want to evaluate? ', resolve)
+    )
+    const params = { expression: expression }
+    ws.send(JSON.stringify({id: id++, method, params}))
+  }
+}
