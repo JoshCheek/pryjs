eed8c1f37e8e36b1c0cb38166bb69bc2122c51d3
Josh Cheek <josh.cheek@gmail.com>
2019-07-11 15:50:19 -0500

    Whoops!

--- example.js
+++ example.js
@@ -1,9 +1,8 @@
 const pry = require('./src')
 
 void async function main () {
-  a = 123
+  let a = 123
   console.log({ a })
-  // await pry({ logLevel: 'silly' })
   await pry()
   console.log({ a })
 }()
